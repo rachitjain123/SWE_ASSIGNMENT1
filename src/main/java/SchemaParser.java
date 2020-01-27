2490eb9d49d2922549f95f9b46b3cfa5551f0f22
Rachit Jain <jainrachit100@gmail.com>
2020-01-27 20:53:30 +0530

    Fixed SLA principle in a method

--- src/main/java/SchemaParser.java
+++ src/main/java/SchemaParser.java
@@ -3,6 +3,7 @@ import java.util.*;
 class SchemaParser {
 
     private Map<Character, ArgumentMarshaller> marshaller;
+    private Map<String, AbstractArgumentMarshaller> allSupportedArguments;
 
     SchemaParser() {
         marshaller = new HashMap<>();
@@ -17,18 +18,22 @@ class SchemaParser {
             throw new ArgsException(ErrorCode.INVALID_ARGUMENT_NAME, elementId);
     }
 
-    private void parseSchemaElement(String element) throws ArgsException {
-        SupportedArguments supportedArguments = new SupportedArguments();
-        Map<String, AbstractArgumentMarshaller> allSupportedArguments = supportedArguments.getSupportedArguments();
-        char elementId = element.charAt(0);
-        String elementTail = element.substring(1);
-        validateSchemaElementId(elementId);
+    private void putIntoMarshaller(String elementTail, char elementId) throws ArgsException {
         if (elementTail.isEmpty())
             marshaller.put(elementId, new BooleanArgumentMarshaller());
         else if (allSupportedArguments.containsKey(elementTail))
             marshaller.put(elementId, allSupportedArguments.get(elementTail));
         else
             throw new ArgsException(ErrorCode.INVALID_ARGUMENT_FORMAT, elementId, elementTail);
+    }
+
+    private void parseSchemaElement(String element) throws ArgsException {
+        SupportedArguments supportedArguments = new SupportedArguments();
+        allSupportedArguments = supportedArguments.getSupportedArguments();
+        char elementId = element.charAt(0);
+        String elementTail = element.substring(1);
+        validateSchemaElementId(elementId);
+        putIntoMarshaller(elementTail, elementId);
     }
 
     void parseSchema(String schema) throws ArgsException {
